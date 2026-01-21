30b8ded5197d96bba32d77713b7a2b24579344ee
CHIZHINIME CHIJIOKE <131488218+chizhinime@users.noreply.github.com>
2026-01-21 13:55:39 +0100

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript, DocumentContext } from "next/document";
+import Script from 'next/script'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -10,14 +11,14 @@ render(){
   return (
     <Html lang="en">
       <Head>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1469724692996264"
-     crossorigin="anonymous"
-          ></script>
         <meta name="google-adsense-account" content="ca-pub-1469724692996264">
       </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1469724692996264"
+     crossorigin="anonymous"
+          />
       </body>
     </Html>
   );
