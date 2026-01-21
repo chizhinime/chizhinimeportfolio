4afdace050dda97354906f608d9c75ae98618349
CHIZHINIME CHIJIOKE <131488218+chizhinime@users.noreply.github.com>
2026-01-21 13:41:19 +0100

    Include Google Ads script in _document.tsx
    Added Google Ads script to the document head.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1469724692996264"
+     crossorigin="anonymous"
+          ></script>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
