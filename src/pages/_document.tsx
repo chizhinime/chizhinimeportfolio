aa7b02fcbe5430f5d289bf06a1fa9a43b26db986
CHIZHINIME CHIJIOKE <131488218+chizhinime@users.noreply.github.com>
2026-01-21 13:50:09 +0100

    Add Google AdSense meta tag to document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,6 +13,7 @@ render(){
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1469724692996264"
      crossorigin="anonymous"
           ></script>
+        <meta name="google-adsense-account" content="ca-pub-1469724692996264">
       </Head>
       <body className="antialiased">
         <Main />
