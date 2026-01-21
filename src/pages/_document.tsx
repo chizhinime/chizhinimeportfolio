4a9a23a8e95b8712cdc063808e75388ebfffe6a0
CHIZHINIME CHIJIOKE <131488218+chizhinime@users.noreply.github.com>
2026-01-21 13:49:30 +0100

    Refactor Document component to use class syntax

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps}
+}
+
+render(){
   return (
     <Html lang="en">
       <Head>
@@ -15,3 +21,5 @@ export default function Document() {
     </Html>
   );
 }
+
+export default MyDocument
